b57eb2116b26df139f5ebb792b1ae0bc5932b333
Tharin Deelaka Mahale <tharin.20200709@iit.ac.lk>
2022-03-24 17:50:46 +0530

    do unit tests

--- testReactTruffle/test/simplestorage.js
+++ testReactTruffle/test/simplestorage.js
@@ -1,15 +1,11 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+  
+  let instance;
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+  beforeEach("should setup the contract instance", async () => {
 
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
-
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    instance = await SimpleStorage.deployed();
   });
 });

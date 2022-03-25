ab7f10e4c52ced0ef4187fdc4368d607b6a8a338
Tharin Deelaka Mahale <tharin.20200709@iit.ac.lk>
2022-03-25 17:46:28 +0530

    yml test

--- testReactTruffle/test/simplestorage.js
+++ testReactTruffle/test/simplestorage.js
@@ -9,5 +9,18 @@ contract("SimpleStorage", accounts => {
     instance = await SimpleStorage.deployed();
   });
 
-  
+  it("should get the value cookies", async () => {
+
+    const cookiesVar = await instance.get();
+    
+    assert.equal(cookiesVar, "cookies");
+  });
+
+  it("should set the value candy", async () => {
+
+    await instance.set("candy");
+    const candyVar = await instance.get();
+    
+    assert.equal(candyVar, "candy");
+  });
 });
